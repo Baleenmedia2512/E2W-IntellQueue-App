6c8d038ee4c7719fd42c13612357a1a908c4eb97
Siva12443 <siva12443@outlook.com>
2023-02-18 10:34:12 +0530

    react.js table files

---
+++ react test/example.js
@@ -0,0 +1,81 @@
+// Author https://github.com/yegor-sytnyk/movies-list
+var i = 1000;
+export default [
+    
+    {
+      id: 1,
+      title: i,
+      CSE_name: "Usha",
+    },
+    {
+      id: 2,
+      title: ++i,
+      CSE_name: "Monisha",
+    },
+    {
+      id: 3,
+      title: ++i,
+      CSE_name: "Usha",
+    },
+    {
+      id: 4,
+      title: ++i,
+      CSE_name: "Monisha",
+    },
+    {
+      id: 5,
+      title: ++i,
+      CSE_name: "Monisha",
+    },
+    {
+      id: 6,
+      title: ++i,
+      CSE_name: "Usha",
+    },
+    {
+      id: 7,
+      title: ++i,
+      CSE_name: "Monisha",
+    },
+    {
+      id: 8,
+      title: ++i,
+      CSE_name: "Monisha",
+   
+    },
+    {
+      id: 9,
+      title: ++i,
+      CSE_name: "Usha",
+    },
+    {
+      id: 10,
+      title: ++i,
+      CSE_name: "Usha"
+    },
+    {
+      id: 11,
+      title: ++i,
+      CSE_name: "Usha",
+    },
+    {
+      id: 12,
+      title: ++i,
+      CSE_name: "Monisha",
+    },
+    {
+      id: 13,
+      title: ++i,
+      CSE_name: "Monisha",
+    },
+    {
+      id: 14,
+      title: ++i,
+      CSE_name: "Usha",
+    },
+    {
+      id: 15,
+      title:++i,
+      CSE_name: "Usha",
+    },
+]
