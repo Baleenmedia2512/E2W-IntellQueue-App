a75557deaa1419e2aca480bf71e4f50fcbe7df7d
Siva12443 <siva12443@outlook.com>
2023-02-26 21:43:10 +0530

    New controller API and DataContext class added

---
+++ Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BaleenWebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<EnquiryTable> Enquiries { get; set; }
+    }
+}
---
+++ EnquiryTable.cs
@@ -0,0 +1,19 @@
+﻿namespace BaleenWebAPI
+{
+    public class EnquiryTable
+    {
+// public int ID { get; set; }
+        public DateTime EntryDateTime { get; set; } 
+        public string EntryUser { get; set; } = string.Empty;
+        public string ClientContact { get; set; } = string.Empty;
+        public string CSE { get; set; } = string.Empty;
+        public int Attended { get; set; }
+        public int Validity { get; set; }
+        public DateTime AttendedDateTime { get; set; }
+        public string Source { get; set; } = string.Empty;
+        public string ReasonForInvalidity { get; set; } = string.Empty;
+        public string ClientName {get; set; } = string.Empty;
+        public string ClientEmail { get; set; } = string.Empty;
+
+    }
+}
