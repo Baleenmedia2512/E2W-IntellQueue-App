8a6924334f43139f2cce2b68a5326ce93f2f8a64
Sivaramakrishnan S <siva12443@outlook.com>
2024-06-01 19:33:16 +0530

    Bottom bar unnexessary icons hidden for Grace Scans

--- app/BottomBar.js
+++ app/BottomBar.js
@@ -13,7 +13,29 @@ const BottomBar = () => {
   const router = useRouter();
   const currentPath = usePathname();
   const [value, setValue] = useState(0); // Define the value state variable
+  const [elementsToHide, setElementsToHide] = useState([])
   const [username, setUsername] = useState(""); // State variable for username
+
+  const elementsToHideList = () => {
+    try{
+      fetch(`https://orders.baleenmedia.com/API/Media/FetchNotVisibleElementName.php/get?JsonDBName=${companyName}`)
+        .then((response) => response.json())
+        .then((data) => setElementsToHide(data));
+    } catch(error){
+      console.error("Error showing element names: " + error)
+    }
+  }
+
+  useEffect(() => {
+    //searching elements to Hide from database
+
+    elementsToHide.forEach((name) => {
+      const elements = document.getElementsByName(name);
+      elements.forEach((element) => {
+        element.style.display = 'none'; // Hide the element
+      });
+    });
+  }, [elementsToHide])
 
   useEffect(() => {
     const fetchUsername = async () => {
@@ -21,6 +43,7 @@ const BottomBar = () => {
       setUsername(fetchedUsername);
     };
     fetchUsername();
+    elementsToHideList()
   }, []);
 
   useEffect(()=>{
@@ -75,18 +98,14 @@ const BottomBar = () => {
   if (currentPath === '/login') {
     return null; // Conditionally return null
   }
-
-  if (username !== "GraceScans") {
-    return null; // Conditionally return null if username is not GraceScans
-  }
-
  
   return (
     <div class="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600">
   <div class="flex justify-evenly h-full max-w-lg mx-auto">
     {/* rate validation button */}
-    <button data-tooltip-target="tooltip-home" 
+    <button data-tooltip-target="tooltip-home"
     type="button"
+    name = "RatesValidationNavigation"
     class="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
     onClick={(e) => handleChange(e, 0)}
     >
@@ -96,42 +115,42 @@ const BottomBar = () => {
 
       <span class="sr-only">Rates Validation</span>
     </button>
-    <div id="tooltip-home" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+    <div id="tooltip-home" name = "RatesValidationNavigation" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
       Rates Validation
       <div class="tooltip-arrow" data-popper-arrow></div>
     </div>
     {/* rate validation button */}
     
 {/* Enquiry button */}
-    <button data-tooltip-target="tooltip-wallet" onClick={(e) => handleChange(e, 1)} type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
+    <button data-tooltip-target="tooltip-wallet" name = "EnquiryNavigation" onClick={(e) => handleChange(e, 1)} type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
       <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
         <path fill-rule="evenodd" d="M9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H7Zm8-1a1 1 0 0 1 1-1h1v-1a1 1 0 1 1 2 0v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 0 1-1-1Z" clip-rule="evenodd"/>
       </svg>
 
       <span class="sr-only">Enquiry</span>
     </button>
-    <div id="tooltip-wallet" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+    <div id="tooltip-wallet" name = "EnquiryNavigation" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
       Enquiry
       <div class="tooltip-arrow" data-popper-arrow></div>
     </div>
   {/* Enquiry button */}
 
   {/* Quote Sender button */}
-    <button data-tooltip-target="tooltip-settings" onClick={(e) => handleChange(e, 2)}type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
+    <button data-tooltip-target="tooltip-settings" name = "QuoteSenderNavigation" onClick={(e) => handleChange(e, 2)}type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
     <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
       <path fill-rule="evenodd" d="M3.559 4.544c.355-.35.834-.544 1.33-.544H19.11c.496 0 .975.194 1.33.544.356.35.559.829.559 1.331v9.25c0 .502-.203.981-.559 1.331-.355.35-.834.544-1.33.544H15.5l-2.7 3.6a1 1 0 0 1-1.6 0L8.5 17H4.889c-.496 0-.975-.194-1.33-.544A1.868 1.868 0 0 1 3 15.125v-9.25c0-.502.203-.981.559-1.331ZM7.556 7.5a1 1 0 1 0 0 2h8a1 1 0 0 0 0-2h-8Zm0 3.5a1 1 0 1 0 0 2H12a1 1 0 1 0 0-2H7.556Z" clip-rule="evenodd"/>
     </svg>
 
       <span class="sr-only">Quote Sender</span>
     </button>
-    <div id="tooltip-settings" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+    <div id="tooltip-settings" name = "QuoteSenderNavigation" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
     Quote Sender
       <div class="tooltip-arrow" data-popper-arrow></div>
     </div>
     {/* Quote Sender button */}
 
     {/* Rates Entry button */}
-    <button data-tooltip-target="tooltip-profile" type="button" onClick={(e) => handleChange(e, 3)} class="inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
+    <button data-tooltip-target="tooltip-profile" name = "RatesEntryNavigation" type="button" onClick={(e) => handleChange(e, 3)} class="inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
     <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
   <path fill-rule="evenodd" d="M7 6a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2h-2v-4a3 3 0 0 0-3-3H7V6Z" clip-rule="evenodd"/>
   <path fill-rule="evenodd" d="M2 11a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-7Zm7.5 1a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5Z" clip-rule="evenodd"/>
@@ -140,20 +159,20 @@ const BottomBar = () => {
 
       <span class="sr-only">Rates Entry</span>
     </button>
-    <div id="tooltip-profile" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+    <div id="tooltip-profile" name = "RatesEntryNavigation" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
     Rates Entry
       <div class="tooltip-arrow" data-popper-arrow></div>
       </div>
       {/* Rates Entry button */}
 
 {/* Finance Entry button */}
-<button data-tooltip-target="tooltip-finance" type="button" onClick={(e) => handleChange(e, 4)} class="inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
+<button data-tooltip-target="tooltip-finance" type="button" onClick={(e) => handleChange(e, 4)} name = "FinanceEntryNavigation" class="inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
   <svg class="w-6 h-6 text-gray-800 dark:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
     <path fill-rule="evenodd" d="M2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0zm8-5a5 5 0 1 0 0 10 5 5 0 0 0 0-10zm0 2a3 3 0 1 0 0 6 3 3 0 0 0 0-6zm-.5 1a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5z"/>
   </svg>
   <span class="sr-only">Finance Entry</span>
 </button>
-<div id="tooltip-finance" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+<div id="tooltip-finance" role="tooltip" name = "FinanceEntryNavigation" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
   Finance Entry
   <div class="tooltip-arrow" data-popper-arrow></div>
 </div>
