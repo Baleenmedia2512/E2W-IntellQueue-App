5dedf3dae82f15a888b26ae6047e08db907038a7
Sivaramakrishnan S <siva12443@outlook.com>
2024-07-17 19:40:27 +0530

    Quote Module released with multiple quotes

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -220,7 +220,7 @@ const handleLogin = (event) => {
                         </button>
                     </form>
                     <div className="text-gray-600 text-xs mt-4">
-                        Version 1.2.1
+                        Version 1.2.2
                     </div>
                 </div>
                 {/* Additional space with curved edges for pictures (visible on larger screens) */}
