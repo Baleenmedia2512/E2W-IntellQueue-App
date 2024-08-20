222ad6bd722267a6a35dfee55e49b9a2f3cbca62
Logeshwaran V <logeshwaran06@outlook.com>
2024-08-20 16:35:56 +0530

    WO - removed unnecessary validation on adjustment

--- app/Create-Order/page.jsx
+++ app/Create-Order/page.jsx
@@ -1410,7 +1410,7 @@ return (
     <div>
       <label className="block text-gray-700 font-semibold mb-2">Adjustment (+/-)</label>
       <input 
-        className={`w-full px-4 py-2 border text-black rounded-lg focus:outline-none focus:shadow-outline focus:border-blue-300 focus:ring focus:ring-blue-300 ${errors.marginAmount ? 'border-red-400' : ''}`}
+        className={`w-full px-4 py-2 border text-black rounded-lg focus:outline-none focus:shadow-outline focus:border-blue-300 focus:ring focus:ring-blue-300`}
         type="number"
         placeholder="Amount Adjustment"
         value={discountAmount || ''}
