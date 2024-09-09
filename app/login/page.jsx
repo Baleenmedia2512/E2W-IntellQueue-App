06c67b82e3adc5679ddfa758bb5d24bef8793721
Asath1304 <asathkameel131@gmail.com>
2024-09-09 17:13:02 +0530

    Version Changed

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -254,7 +254,7 @@ const handleLogin = (event) => {
                         </button>
                     </form>
                     <div className="text-gray-600 text-xs mt-4">
-                        Version 1.7.2
+                        Version 1.7.3
                     </div>
                 </div>
                 {/* Additional space with curved edges for pictures (visible on larger screens) */}
