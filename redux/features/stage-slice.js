86057cb45c984ae01534c1fae6f053f5f7c61be0
KuMaRa2004 <kumara02112004@gmail.com>
2024-09-28 21:09:09 +0530

    stage slice for redux storage to place order

--- redux/features/stage-slice.js
+++ redux/features/stage-slice.js
@@ -1,25 +1,48 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const getCurrentDate = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0'); // Month is 0-indexed, so we add 1
+  const day = String(today.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
+
 const initialState = {
-  stages: [],
+  stages: [{ title: "", description: "", dueDate: getCurrentDate(), stageAmount: "" }],
 };
 
 const stageSlice = createSlice({
-  name: 'stage',
+  name: 'stages',
   initialState,
   reducers: {
     addItemsToStage: (state, action) => {
       state.stages = [...state.stages, ...action.payload];
     },
-    removeItem: (state, action) => {
-      const index = action.payload;
-      state.stages = state.stages.filter(item => item.index !== index);
+    updateStage: (state, action) => {
+      const { index, field, value } = action.payload;
+      state.stages[index][field] = value;
     },
+    removeItem: (state) => {
+      if (state.stages.length > 0) {
+        // Remove the last stage in the array
+        state.stages.pop();
+      }
+    },
+    
     resetStageItem: (state) => {
       return initialState
+    },
+    addStage: (state) => {
+      state.stages.push({ 
+        title: "", 
+        description: "", 
+        dueDate: getCurrentDate(), 
+        stageAmount: "" 
+      });
     }
   },
 });
 
-export const { addItemsToStage, removeItem, resetStageItem } = stageSlice.actions;
+export const { addItemsToStage, removeItem, resetStageItem, updateStage, addStage } = stageSlice.actions;
 export const stageReducer = stageSlice.reducer;
