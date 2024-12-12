983fbee4826fd5494d447afd783767db4df36b89
siva-12443 <siva12443@outlook.com>
2024-12-12 13:10:03 +0530

    Handled By Lead Status Added in Front-End and Backend

--- app/LeadManager/page.jsx
+++ app/LeadManager/page.jsx
@@ -10,6 +10,9 @@ import { FaFileExcel } from "react-icons/fa";
 import { GiCampfire } from "react-icons/gi";
 import { MdOutlineWbSunny } from "react-icons/md";
 import { FaRegSnowflake } from "react-icons/fa";
+import { useAppSelector } from "@/redux/store";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPerson, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
 
 
@@ -59,6 +62,7 @@ const parseFollowupDate = (dateStr) => {
 const EventCards = ({params, searchParams}) => {
   const [rows, setRows] = useState([]);
   const [loading, setLoading] = useState(true);
+  const userName = useAppSelector(state => state.authSlice.userName);
   const [showModal, setShowModal] = useState(false);
   const [currentCall, setCurrentCall] = useState({ phone: "", name: "", sNo: "", Platform: "", Enquiry: "", LeadDateTime: "" });
   const [selectedStatus, setSelectedStatus] = useState("");
@@ -84,7 +88,6 @@ const handleCheckboxChange = () => {
   setHasSaved(true); // Set hasSaved to true when checkbox is checked
 };	
 
-
   const fetchData = async () => {
     try {
       const filters = {
@@ -103,11 +106,10 @@ const handleCheckboxChange = () => {
   };
 
   useEffect(() => {
+    if (!userName) return;
     fetchData();
   }, [params.id, searchParams]);
 
-
-  
   useEffect(() => {
     if (showModal) {
       // Set initial values when the modal is opened
@@ -152,18 +154,20 @@ const handleCheckboxChange = () => {
 
 
     let payload = {};
-  
+
     // Prepare payload based on context
     if (sendQuoteOnly) {
       payload = {
         sNo: Sno,
         quoteSent: quoteSent,
+        handledBy: toTitleCase(userName)
       };
     } else if (followupOnly) {
       payload = {
         sNo: currentCall.sNo,
         followupDate: followupDate || "", // Ensure followupDate is a string or empty
         followupTime: followupTime || "", // Ensure followupTime is a string or empty
+        handledBy: toTitleCase(userName)
       };
     } else {
       payload = {
@@ -175,6 +179,7 @@ const handleCheckboxChange = () => {
         quoteSent: quoteSent || "",
         remarks: remarks || "",
         prospectType: prospectType || "",  // Include ProspectType
+        handledBy: toTitleCase(userName)
       };
     }
   
@@ -377,6 +382,14 @@ const handleCheckboxChange = () => {
               >
                 {row.Status}
               </span>
+              {row.HandleBy && (
+              <div className="text-xs mt-2 p-1 justify-start px-3 hover: cursor-pointer text-white bg-gradient-to-r from-blue-500 to-blue-700 rounded-full flex flex-row border border-blue-500">
+                <FontAwesomeIcon icon={faUserCircle} className="mr-1 mt-[0.1rem]"/>
+                <p className="font-poppins">
+                  {row.HandleBy}
+                </p>
+              </div>
+            )}
             </div>
 
             <div className="absolute top-2 left-2 flex flex-row">
@@ -493,8 +506,10 @@ const handleCheckboxChange = () => {
                 </p>
               </div>
             )}
+            
           </div>
         ))}
+        
       </div>
 
       {/* Modal for Call Status */}
