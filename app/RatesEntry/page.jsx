2a3a69c85175187b1feaae7d28966bba38d85df1
Logeshwaran V <logeshwaran67677@gmail.com>
2024-12-20 11:27:11 +0530

    Bug fixed RM: Remove/Restore pops up while scrolling the suggestions for mobile.

--- app/RatesEntry/page.jsx
+++ app/RatesEntry/page.jsx
@@ -1556,7 +1556,7 @@ const handleLongPress = (rate, isInvalid) => {
     setLongPressRateId(rate);
     setDialogAction(isInvalid ? 'restore' : 'remove');
     setOpenDialog(true);
-  }, 600); 
+  }, 1000); 
 };
 
 const handleTouchEnd = () => {
