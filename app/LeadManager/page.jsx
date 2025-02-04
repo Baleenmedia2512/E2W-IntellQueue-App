7372c8e0aa04071a06a8ae60af9748d22ee087f0
siva-12443 <siva12443@outlook.com>
2025-02-04 15:01:27 +0530

    leadssss

--- app/LeadManager/page.jsx
+++ app/LeadManager/page.jsx
@@ -1098,7 +1098,6 @@ const EventCards = ({params, searchParams}) => {
             </div>
             )}
 
-
             <div className="flex justify-end">
             <button
               className={`px-4 py-2 rounded-md text-white ${
