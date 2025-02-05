d0dfe0c4e1887206b44a8fa99251e5c447902eb2
Logeshwaran V <logeshwaran67677@gmail.com>
2025-02-05 16:38:01 +0530

    For Deployment

--- app/Report/ConsultantReport/page.jsx
+++ app/Report/ConsultantReport/page.jsx
@@ -404,7 +404,7 @@ const handleMarkAsUnprocessed = async () => {
         if (typeof rowData.rateCard === 'string' && rowData.rateCard.startsWith('Total')) {
             return <span className="font-bold text-blue-500">₹{rowData.price}</span>;
         }
-        return <span>₹{rowData.price}</span>; // Ensure this displays properly if it's a number
+        return <span>₹{rowData.price}</span>;
     };
 
     const consultantBodyTemplate = (rowData) => {
