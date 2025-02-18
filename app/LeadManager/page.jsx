aaa8977988ef31e1fc739273022b1be4e6a20682
siva-12443 <siva12443@outlook.com>
2025-02-18 15:16:51 +0530

    New lead insert

--- app/LeadManager/page.jsx
+++ app/LeadManager/page.jsx
@@ -1345,7 +1345,6 @@ console.log("handle by",formData.handledBy)
         <AiOutlinePlus size={24} />
       </button>
 
-
       {/* Modal Form */}
       {isModalOpen && (
         <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50">
