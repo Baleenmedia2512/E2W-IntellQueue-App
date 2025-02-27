e5cf9724af700f25cdbdbd4b9cc07c6023e5e0ad
siva-12443 <siva12443@outlook.com>
2025-02-27 12:52:11 +0530

    For deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withPWA = require('next-pwa')({
-  dest: 'public'
-})
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in development
+});
 
 module.exports = withPWA({
-})
+  reactStrictMode: true, // Add other Next.js config options here
+});
