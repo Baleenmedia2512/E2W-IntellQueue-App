01061c1539de8c02abc5e4d6534a90c9f40e6dd0
Siva-12443 <siva12443@outlook.com>
2025-03-13 16:52:56 +0530

    Pushing for deployment

--- app/api/InsertUpdateAPI.jsx
+++ app/api/InsertUpdateAPI.jsx
@@ -22,3 +22,22 @@ export const PostInsertOrUpdate = async (endpoint, data) => {
 
     return result;
 };
+
+export const GetInsertOrUpdate = async (endpoint, queryParams = {}) => {
+    let result = [];
+    try {
+      const response = await api.get(`${endpoint}.php`, {
+        params: queryParams
+      });
+      result = response.data;
+    } catch (error) {
+      console.error(error);
+      if (error.response && error.response.data) {
+        result = `Error: ${JSON.stringify(error.response.data)}`;
+      } else {
+        result = `Error while fetching data: ${error.message}`;
+      }
+    }
+    return result;
+  };
+  
