3bf6d9f55df63f244b55d6ca9a7955084050d138
Logeshwaran V <logeshwaran67677@gmail.com>
2025-05-02 11:32:29 +0530

    Revert "QS: Removed Logs" Reverting last release This reverts commit 946406cd0ca08f028a98ab228b1d527710a2785d.

--- app/QueueSystem/WaitingScreen/page.jsx
+++ app/QueueSystem/WaitingScreen/page.jsx
@@ -13,7 +13,8 @@ export default function WaitingScreen() {
     const [queuePosition, setQueuePosition] = useState(null);
     const [waitingTime, setWaitingTime] = useState(null);
     const [totalOrders, setTotalOrders] = useState(0);
-    
+    console.log(queuePosition)
+
     useEffect(() => {
         if (!companyName || !phoneNumber) {
             router.push('/QueueSystem/InvalidAccess');
