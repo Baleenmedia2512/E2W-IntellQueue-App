89ca6a4bafac6c4deaf375ab26086985022960eb
Siva-12443 <siva12443@outlook.com>
2025-05-24 13:11:38 +0530

    DB Config for local files changed to localhost

--- api/DBConfig.php
+++ api/DBConfig.php
@@ -2,28 +2,29 @@
 return [
     'Grace Scans' => [
         'servername' => 'localhost',
-        'username' => 'baleeed5_gracescans',
-        'password' => 'Grace@123#',
+        'username' => 'root',
+        'password' => '',
         'dbname' => 'baleeed5_gracescans',
     ],
     'Baleen Media' => [
         'servername' => 'localhost',
-        'username' => 'baleeed5_web_adm',
-        'password' => 'Baleen@789#',
+        'username' => 'root',
+        'password' => '',
         'dbname' => 'baleeed5_live',
     ],
     'Easy2Work' => [
         'servername' => 'localhost',
-        'username' => 'baleeed5_easy2work',
-        'password' => 'Easy@123#',
+        'username' => 'root',
+        'password' => '',
         'dbname' => 'baleeed5_easy2work'
     ],
     'Baleen Test' => [
         'servername' => 'localhost',
-        'username' => 'baleeed5_test',
-        'password' => 'Baleen@789#',
-        'dbname' => 'baleeed5_test_e2w',
-    ]
+        'username' => 'root',
+        'password' => '',
+        'dbname' => 'baleeed5_test_e2w',  // Replace this with your actual local DB name
+        'port' => 3306                // Explicitly define port
+    ],
     // Add more database configurations as needed
 ];
 ?>
