f1edbbfced65a0bdf80e04958f4eee025067b367
Logeshwaran V <logeshwaran67677@gmail.com>
2025-05-24 11:21:12 +0530

    FetchConsultantReport.php: Additionally returns Consultant Place

--- api/API/Media/FetchConsultantReport.php
+++ api/API/Media/FetchConsultantReport.php
@@ -22,7 +22,8 @@ if ($start_date && $end_date) {
     $queryBase = "";
 
     if ($showProcessedConsultantsOnly) {
-        $queryBase .= "SELECT ROW_NUMBER() OVER (ORDER BY o.ConsultantName, o.Card, o.AdType) AS id, 
+        $queryBase .= "SELECT 
+                        ROW_NUMBER() OVER (ORDER BY o.ConsultantName, o.Card, o.AdType) AS id, 
                         o.ConsultantName AS name, 
                         o.Card AS rateCard, 
                         o.AdType AS rateType,
@@ -30,16 +31,18 @@ if ($start_date && $end_date) {
                         o.OrderNumber AS orderNumber,
                         o.ClientName AS clientName,
                         o.RateWiseOrderNumber AS rateWiseOrderNumber,
-                        o.ConsultantId
-                        FROM order_table o 
-                        WHERE o.OrderDate BETWEEN :startDate AND :endDate
-                       AND o.ConsultantName != ''
-                       AND o.OrderNumber > 0
-                       AND o.CancelFlag = 0
-                       AND o.Commission != 0
-                       AND o.IncentiveProcessedOn != '0000-00-00'
-                       GROUP BY o.OrderNumber
-                       ORDER BY name, rateCard, rateType;";
+                        o.ConsultantId,
+                        c.ConsultantPlace
+                   FROM order_table o
+                   LEFT JOIN consultant_table c ON o.ConsultantId = c.CId
+                   WHERE o.OrderDate BETWEEN :startDate AND :endDate
+                     AND o.ConsultantName != ''
+                     AND o.OrderNumber > 0
+                     AND o.CancelFlag = 0
+                     AND o.Commission != 0
+                     AND o.IncentiveProcessedOn != '0000-00-00'
+                   GROUP BY o.OrderNumber
+                   ORDER BY name, rateCard, rateType;";
     } else {
         $queryBase .= "SELECT 
                         ROW_NUMBER() OVER (ORDER BY o.ConsultantName, o.Card, o.AdType) AS id, 
@@ -50,18 +53,20 @@ if ($start_date && $end_date) {
                         o.OrderNumber AS orderNumber,
                         o.ClientName AS clientName,
                         o.RateWiseOrderNumber AS rateWiseOrderNumber,
-                        o.ConsultantId
-                    FROM order_table o
-                    JOIN financial_transaction_table ftt ON o.OrderNumber = ftt.OrderNumber
-                    WHERE o.OrderDate BETWEEN :startDate AND :endDate
-                        AND o.ConsultantName != ''
-                        AND o.OrderNumber > 0
-                        AND o.CancelFlag = 0
-                        AND o.Commission != 0
-                        AND o.IncentiveProcessedOn = '0000-00-00'
-                        AND ftt.ValidStatus = 'Valid'
-                    GROUP BY o.OrderNumber
-                    ORDER BY name, rateCard, rateType;";
+                        o.ConsultantId,
+                        c.ConsultantPlace
+                   FROM order_table o
+                   JOIN financial_transaction_table ftt ON o.OrderNumber = ftt.OrderNumber
+                   LEFT JOIN consultant_table c ON o.ConsultantId = c.CId
+                   WHERE o.OrderDate BETWEEN :startDate AND :endDate
+                     AND o.ConsultantName != ''
+                     AND o.OrderNumber > 0
+                     AND o.CancelFlag = 0
+                     AND o.Commission != 0
+                     AND o.IncentiveProcessedOn = '0000-00-00'
+                     AND ftt.ValidStatus = 'Valid'
+                   GROUP BY o.OrderNumber
+                   ORDER BY name, rateCard, rateType;";
     }
 
     $stmt = $pdo->prepare($queryBase);
