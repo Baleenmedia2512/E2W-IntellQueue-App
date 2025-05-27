c837da6b5ba14ce4dd6231078681c96145f341d9
Siva-12443 <siva12443@outlook.com>
2025-05-27 09:46:29 +0530

    Refactor: Enable 'use client' directive in LeadManager page and update service worker map file paths

--- app/LeadManager/page.jsx
+++ app/LeadManager/page.jsx
@@ -1,4 +1,4 @@
-// 'use client'
+'use client'
 // import { useEffect, useState } from "react";
 // import DatePicker from "react-datepicker";
 // import "react-datepicker/dist/react-datepicker.css";
