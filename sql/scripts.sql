d5757c583ecfbf7e45c57f5cd7cbfffe4a58f8aa
Logesh6767 <logeshwaran67677@gmail.com>
2025-06-02 18:56:33 +0530

    sql: Latest db scripts

--- sql/scripts.sql
+++ sql/scripts.sql
@@ -1,6 +1,10 @@
--- 31-05-2025 - Logesh
+-- 31-05-2025 - Logesh - test_e2w
 CREATE TABLE fcm_token_table (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     EntryDateTime DATETIME DEFAULT CURRENT_TIMESTAMP,
     Token VARCHAR(512) NOT NULL UNIQUE
 );
+
+-- 02-05-2025 - Logesh - test_e2w
+TRUNCATE TABLE queue_history_table;
+TRUNCATE TABLE queue_table;
