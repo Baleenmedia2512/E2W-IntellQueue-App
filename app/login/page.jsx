06dc221360811a51ce25701362a5ff03d3ce125e
Logesh6767 <logeshwaran67677@gmail.com>
2025-06-02 13:31:40 +0530

    Changed cred to test

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -89,7 +89,7 @@ const handleLogin = (event) => {
     if (validateFields()) {
         const encodedPassw = encodeURIComponent(password);
 
-        fetch(`https://orders.baleenmedia.com/API/Media/Login.php/get?JsonDBName=${companyName}&JsonUserName=${userName}&JsonPassword=${encodedPassw}`)
+        fetch(`https://orders.baleenmedia.com/API/Media/Login.php/get?JsonDBName=${'Baleen Test'}&JsonUserName=${userName}&JsonPassword=${encodedPassw}`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error(response.statusText);
@@ -99,10 +99,10 @@ const handleLogin = (event) => {
             .then(data => {
                 if (data.status === 'Login Successfully') {
                     setSuccessMessage('Login Successful!');
-                    // dispatch(setDBName(companyName));
-                    // dispatch(setCompanyName('Baleen Test'))
-                    // dispatch(login(userName));
                     dispatch(setDBName(companyName));
+                    dispatch(setCompanyName('Baleen Test'))
+                    // dispatch(login(userName));
+                    // dispatch(setDBName(companyName));
                     setTimeout(() => {
                         setSuccessMessage('');
                         router.push("/")
@@ -110,7 +110,7 @@ const handleLogin = (event) => {
 
                     // Dispatch actions and navigate based on conditions
                     
-                    dispatch(setCompanyName(companyName))
+                    // dispatch(setCompanyName(companyName))
                     dispatch(login(userName));
                     
                     dispatch(setAppRights(data.appRights));
