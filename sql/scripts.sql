a72d83f3fed00982da0a9b344a420295a026e5f3
siva-12443 <siva12443@outlook.com>
2025-06-03 09:31:21 +0530

    Revert "commented already executed script"
    This reverts commit a04a5889f1fae3ba00ee35183287f77d6572d4f9.

--- sql/scripts.sql
+++ sql/scripts.sql
@@ -1,4 +1,4 @@
 --Following scripts need to be run to add two new columns to the cart_table in baleeed5_live database
---ALTER TABLE cart_table ADD COLUMN Edition VARCHAR(128); --Create a new column for Edition in cart_table
---ALTER TABLE cart_table ADD COLUMN Package VARCHAR(128); --Create a new column for Package in cart_table
+ALTER TABLE cart_table ADD COLUMN Edition VARCHAR(128); --Create a new column for Edition in cart_table
+ALTER TABLE cart_table ADD COLUMN Package VARCHAR(128); --Create a new column for Package in cart_table
 ALTER TABLE cart_table ADD COLUMN ValidityDate DATE;--Create a new column for ValidityDate in cart_table
